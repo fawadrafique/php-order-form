1b4dc7957b0cae86b9c9e4bcdc2812e32f8889e8
Muhammad Fawad Rafique <fawadrafique@hotmail.com>
2020-09-11 18:50:31 +0200

    Clean format code

--- email.php
+++ email.php
@@ -61,7 +61,7 @@ $message = '
     <br />
   </body>
 </html>';
-$from = owner_email;
+$from = dp_email;
 // To send HTML mail, the Content-type header must be set
 $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
